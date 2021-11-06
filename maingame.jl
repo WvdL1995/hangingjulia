73917afac62e9e8d0d1f7785cfa95ef8d965a93d
WvdL1995 <w.vanderlisse@gmail.com>
2021-11-06 13:18:18 +0100

    first interface functions,trouble with variables

--- maingame.jl
+++ maingame.jl
@@ -13,14 +13,43 @@ function getguess(alphabet)
         println("What letter would you want to try?")
         guess = readline()
         if sizeof(guess)==1 && occursin(lowercase(guess),alphabet)
-            return guess
+            return lowercase(guess)
         else
             println("That did not work, try again!")
         end
     end
 end
-    
-word = wordchoice()
+function displaydiscovery(word,alphabet,guessed)
+    guessedlist = collect(guessed) #list guesses
+    notguessed=collect(replace(alphabet,guessedlist => "")) #remove guessed letters
+    viewer = replace(word,notguessed =>"_") #remove all letters from word that are not guessed
+    return viewer    
+end
+function interface(word,guess,guessed,alphabet)
+    println("           Word: ",displaydiscovery(word,alphabet,guessed))
+    println("           Guess: ",guess)
+    guessedlist = collect(guessed) #list guesses
+    inwordlist = collect(word)
+    misses = collect(replace(guessedlist,inwordlist =>""))
+    println("           Misses: ",misses)
+end
+
+
+#start game
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-guess = getguess(alphabet)
-print(guessinword(word,guess))
+word = wordchoice()
+guessed = ""
+println(displaydiscovery(word,alphabet,guessed))
+guess=getguess(alphabet)
+guessed = join(guessed,guess)
+print(guessed)
+interface(word,guess,guessed,alphabet)
+
+
+
+# print(guessinword(word,guess))
+
+#interface = "  ____\n |    |\n\O/   |\n\n |    |\n/ \   |\n  ____|____"
+
+
+
